6b96f5529ab1675a40a4ab44516c757d81974ba2
Ben Zhang <benzhangniu+gpg-rpi@gmail.com>
2023-07-25 20:48:14 +0000

    Implement agent

---
+++ agent/README.md
@@ -0,0 +1,6 @@
+## Getting started
+
+```bash
+pip install -r requirements.txt
+sudo -E python3 main.py
+```
--- agent/main.py
+++ agent/main.py
@@ -1,39 +1,65 @@
+import requests
 import RPi.GPIO as GPIO
 import time
 import sys
+import socket
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+hostname = socket.gethostname()
 
 # Define GPIO pin numbers
 relay1 = 4
-relay2 = 22
-relay3 = 6
-relay4 = 26
+# kept for future use
+#relay2 = 22
+#relay3 = 6
+#relay4 = 26
 
 # Use the Broadcom SOC channel
 GPIO.setmode(GPIO.BCM)
 
-# Set up GPIO pins
+## Set up GPIO pins
 GPIO.setup(relay1, GPIO.OUT)
-GPIO.setup(relay2, GPIO.OUT)
-GPIO.setup(relay3, GPIO.OUT)
-GPIO.setup(relay4, GPIO.OUT)
 
-print("Testing relays!", file=sys.stderr)
+logging.info("Initializing relay to off")
+GPIO.output(relay1, GPIO.LOW)
 
+relay1_on = False
+
+# Continuously poll for the latest command
 try:
-    print("Turning on relays", file=sys.stderr)
-    GPIO.output(relay1, GPIO.HIGH)
-    GPIO.output(relay2, GPIO.HIGH)
-    GPIO.output(relay3, GPIO.HIGH)
-    GPIO.output(relay4, GPIO.HIGH)
-    print("Turned on relays", file=sys.stderr)
     while True:
-        time.sleep(10)
-finally:
-    print("Turning off relays", file=sys.stderr)
-    GPIO.output(relay1, GPIO.LOW)
-    GPIO.output(relay2, GPIO.LOW)
-    GPIO.output(relay3, GPIO.LOW)
-    GPIO.output(relay4, GPIO.LOW)
+        try:
+            #url = 'http://100.106.129.114:8081/api/take_command' # for debugging
+            url = 'https://gate-opener-cloud.fly.dev/api/take_command'
+            data = {'host': hostname}
+        
+            response = requests.post(url, json=data)
 
-    print("Cleaning up!", file=sys.stderr)
+            command = response.text
+            logging.info(f"Command: {command}")
+        
+            if command == 'closed':
+                logging.info(f"Turning off relay if on ({relay1_on=})")
+                if relay1_on:
+                    GPIO.output(relay1, GPIO.LOW)
+            elif command == 'open':
+                logging.info(f"Turning on relay if off ({relay1_on=})")
+                if not relay1_on:
+                    GPIO.output(relay1, GPIO.HIGH)
+            else:
+                raise ValueError(f"Unknown command: {commandl}")
+        
+            time.sleep(1)
+        except Exception as e:
+            logging.error(f"Unexpected {err=}, {type(err)=}")
+            logging.warning("Sleeping for 10 seconds and trying again")
+            time.sleep(10)
+finally:
+    logging.info("Turning off relays")
+    GPIO.output(relay1, GPIO.LOW)
+
+    logging.info("Cleaning up!")
     GPIO.cleanup()
+
