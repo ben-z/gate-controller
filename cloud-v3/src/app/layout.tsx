6828a22609280ad3c5053ebbe88d6a0c29ce4c42
Ben Zhang <5977478+ben-z@users.noreply.github.com>
2025-04-01 00:21:29 -0700

    Request persistent storage on the client (#11)
    This PR adds a request for persistent storage for cloud-v3. This make it
    so that clients can have long-lived localStorage and cookies. This is
    useful because we want to install the website on users' devices as an
    app.
    
    This works in Firefox, but not Chrome or Safari. On this page it says
    Chrome automatically handles this request:
    https://web.dev/articles/persistent-storage . Perhaps Safari does the
    same.

--- cloud-v3/src/app/layout.tsx
+++ cloud-v3/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from './providers';
+import ClientInit from '@/components/client-init';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ClientInit />
         <Providers>{children}</Providers>
       </body>
     </html>
