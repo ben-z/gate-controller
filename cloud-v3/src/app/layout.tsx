16c39a27a778ba09898f2083cb80bc34fae3b38b
Ben Zhang <benzhangniu@gmail.com>
2025-04-01 17:28:47 -0700

    Add better support for apple devices by: - Using black-translucent for PWAs - Add favicon.ico and apple-icon.png to src/app for automatic optimization (https://nextjs.org/docs/app/api-reference/file-conventions/metadata/app-icons) - Add startup image - though this is not working in local testing. Not sure if it will work in prod.

--- cloud-v3/src/app/layout.tsx
+++ cloud-v3/src/app/layout.tsx
@@ -1,12 +1,31 @@
+import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'Gate Controller',
-  description: 'A web interface for controlling an automated gate',
+export const metadata: Metadata = {
+  title: "Gate Controller",
+  description: "A web interface for controlling an automated gate",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+    startupImage: [
+      {
+        url: "/apple-touch-startup-image.png",
+      },
+      {
+        url: "/apple-touch-startup-image-dark.png",
+        media: "screen and(prefers-color-scheme: dark)",
+      },
+    ],
+  },
+  other: {
+    // Needed for splash screen on iOS because of this bug:
+    // https://github.com/vercel/next.js/issues/74524#issuecomment-2765443131
+    "apple-mobile-web-app-capable": "yes",
+  },
 };
 
 export default function RootLayout({
